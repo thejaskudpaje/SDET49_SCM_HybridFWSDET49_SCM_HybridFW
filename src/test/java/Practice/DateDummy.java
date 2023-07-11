d50a6d4a419fe338eb4a4209288a988fe13015c9
thejaskudpaje <117477914+thejaskudpaje@users.noreply.github.com>
2023-07-11 13:06:44 +0530

    Update DateDummy.java

--- src/test/java/Practice/DateDummy.java
+++ src/test/java/Practice/DateDummy.java
@@ -22,6 +22,7 @@ System.out.println(d);
 int day = c.get(Calendar.DATE);
 System.out.println(day);
 Calendar cal = Calendar.getInstance();
+	//welcome to git
 int date = cal.get(Calendar.DAY_OF_MONTH);
 System.out.println(date);
 javautility ju=new javautility();
